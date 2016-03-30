a98a3c51afc584f273cd547abf67c07b93f90cfc
EllenJ <something2buy1@gmail.com>
2016-03-30 17:42:11 -0400

    inital commit

---
+++ src/org/ellen/kusuma/dto/HibernateTest.java
@@ -0,0 +1,83 @@
+package org.ellen.kusuma.dto;
+
+import java.util.Date;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		UserDetails user = new UserDetails();
+//		user.setUserId(1);
+		user.setUserName("First User");
+		
+		Vehicle vehicle=new Vehicle();
+		vehicle.setVehicleName("Car");
+		
+		Vehicle vehicle2=new Vehicle();
+		vehicle2.setVehicleName("Jeep");
+		
+		user.getVehicle().add(vehicle);
+		user.getVehicle().add(vehicle2);
+		vehicle.getUserList().add(user);
+		
+		vehicle2.getUserList().add(user);
+
+//		user.setAddress("first user's address");
+//		user.setJoinedDate(new Date());
+//		user.setDescription("description of first user ");	
+		
+//		UserDetails user2=new UserDetails();
+		
+		Address addr = new Address();
+		addr.setStreet("first street");
+		addr.setCity("first city");
+		addr.setState("first state");
+		addr.setPincode("first pincode");
+		
+		Address addr1=new Address();
+
+		addr1.setStreet("first street");
+		addr1.setCity("first city");
+		addr1.setState("first state");
+		addr1.setPincode("first pincode");
+		
+//		user.getListAddress().add(addr);
+//		user.getListAddress().add(addr1);
+		
+//		addr.setStreet("street name");
+//		addr.setCity("city name");
+//		user.setHomeAddress(addr);
+//		user.setOffAddr(addr1);
+//		user2.setUserName("second user");
+		SessionFactory sessionFactory=new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(user);
+		session.save(vehicle);
+		session.save(vehicle2);
+
+//		session.save(user2);
+		session.getTransaction().commit();
+		session.close();
+		
+//		user = null;
+//		session = sessionFactory.openSession();
+////		session.beginTransaction();
+//		user = (UserDetails)session.get(UserDetails.class, 1);
+//		session.close();
+//		System.out.println(user.getListAddress().size());
+		
+//		System.out.println("user Name retrived is " + user.getListAddress());
+//		System.out.println("user name retrieved is" +user.getUserName());
+
+	    }
+	}
+
+
---
+++ src/org/ellen/kusuma/dto/UserDetails.java
@@ -0,0 +1,110 @@
+package org.ellen.kusuma.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.CollectionId;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table (name="USER_DETAILS")
+public class UserDetails {
+	@Id @GeneratedValue(strategy=GenerationType.AUTO)
+
+	private int userId;
+	private String userName;
+	@ManyToMany
+
+	private Collection<Vehicle> vehicle= new ArrayList<Vehicle>();
+	
+//	@ElementCollection(fetch=FetchType.EAGER)
+//	@JoinTable(name="USER_ADDRESS",
+//				joinColumns=@JoinColumn(name="USER_ID")
+//	)
+////	@GenericGenerator(name = "hilo-gen", strategy = "hilo")
+////	@CollectionId(columns={@Column(name="ADDRESS_ID")},generator="hilo-gen",type=@Type(type="long"))
+//	private Collection<Address> listAddress = new ArrayList();
+//	
+//	
+//	public Collection<Address> getListAddress() {
+//		return listAddress;
+//	}
+//	public void setListAddress(Collection<Address> listAddress) {
+//		this.listAddress = listAddress;
+//	}
+	
+
+	/**@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name="street",column=@Column(name="HOME_STREET_NAME")),
+		@AttributeOverride(name="city",column=@Column(name="HOME_CITY_NAME")),
+		@AttributeOverride(name="state",column=@Column(name="HOME_STATE_NAME")),
+		@AttributeOverride(name="pincode",column=@Column(name="HOME_PIN_CODE"))})
+	
+	private Address homeAddress;
+	private Address offAddr;*/
+	
+//	@Temporal(TemporalType.DATE) 
+//	private Date joinedDate;
+//	private String Address;
+//	private String description;
+	
+
+	//	public String getDescription() {
+//		return description;
+//	}
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//	public Date getJoinedDate() {
+//		return joinedDate;
+//	}
+//	public void setJoinedDate(Date joinedDate) {
+//		this.joinedDate = joinedDate;
+//	}
+//	public String getAddress() {
+//		return Address;
+//	}
+//	public void setAddress(String address) {
+//		Address = address;
+//	}
+	public int getUserId() {
+		return userId;
+	}
+public Collection<Vehicle> getVehicle() {
+		return vehicle;
+	}
+	public void setVehicle(Collection<Vehicle> vehicle) {
+		this.vehicle = vehicle;
+	}
+	//	public Address getHomeAddress() {
+//		return homeAddress;
+//	}
+//	public void setHomeAddress(Address homeAddress) {
+//		this.homeAddress = homeAddress;
+//	}
+//	public Address getOffAddr() {
+//		return offAddr;
+//	}
+//	public void setOffAddr(Address offAddr) {
+//		this.offAddr = offAddr;
+//	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+
+
+}
